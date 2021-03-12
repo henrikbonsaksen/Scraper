3eb94fff61b1a23c0dd4f4196f0659a5534eb113
henrikbonsaksen <43421189+henrikbonsaksen@users.noreply.github.com>
2021-03-12 14:03:13 +0100

    selenium code cleanup

--- task2/main.py
+++ task2/main.py
@@ -12,68 +12,37 @@ chrome_options.add_argument('--headless')
 
 driver = webdriver.Chrome(executable_path='./chromedriver', options=chrome_options)
 
-
+start_url = driver.get('https://twitter.com/search?q=%23Winter%27')
 counter = 0
-url = driver.get('https://twitter.com/search?q=%23Winter')
-formated_tags = []
-
-def scrape_twitter(url, counter, formated_tags):
-        try:
-            element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//a[contains(@href,'hashtag')]"))
-            )
-
-        finally:
-            tags = driver.find_elements_by_xpath("//a[contains(@href,'hashtag')]")
-            for tag in tags:
-                print(f"----{tag.get_attribute('href')}")
-                formated_tags.append(tag.get_attribute('href'))
+formatted_tags = []
 
 
-        print(6 - counter, "pages to visit")
+def get_tags(url):
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//a[contains(@href,'hashtag')]")))
 
-        url = random.choice(formated_tags)
-        url = str(url)
-        print("next url to visit: ", str(url))
-        next_url = driver.get(url)
+    except TimeoutException:
+        print("Could not find element")
 
-        while counter < 6:
-            counter += 1
-            scrape_twitter(next_url, counter, formated_tags)
-
-        driver.close()
-
-
-scrape_twitter(url, counter, formated_tags)
-
-
-"""
-
-
-base_url = u'https://twitter.com/search?q='
-query = u'%23Winter'
-url = base_url + query
-browser.get(url)
-
-for _ in range(10):
-    body.send_keys(Keys.PAGE_DOWN)
-    time.sleep(0.2)
-
-tweets = browser.find_elements_by_css_selector("[data-testid=\"tweet\"]")
-
-bucket = {}
-
-for tweet in tweets:
-    bucket['text'] = tweet.text.format()
-    bucket['url'] = tweet.
-    #print(bucket)
-
-    if len(bucket) < 6:
-        # print(tweet.text)
+    finally:
         tags = driver.find_elements_by_xpath("//a[contains(@href,'hashtag')]")
+
         for tag in tags:
             print(f"----{tag.get_attribute('href')}")
-            formated_tags.append(tag.get_attribute('href'))
-            # print(formated_tags)
+            formatted_tags.append(tag.get_attribute('href'))
 
-"""
+    return formatted_tags
+
+
+get_tags(start_url)
+
+while counter < 6:
+    counter += 1
+    print(6 - counter, "pages left to visit")
+    next_url = random.choice(formatted_tags)
+    print("Next page: ", next_url)
+    formatted_tags = []
+    next_page = driver.get(next_url)
+    get_tags(next_page)
+driver.close()
