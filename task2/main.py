8535a7fa419a531ac37eead075e239ed05ff29cd
henrikbonsaksen <43421189+henrikbonsaksen@users.noreply.github.com>
2021-03-12 09:03:38 +0100

    selenium code updates

--- task2/main.py
+++ task2/main.py
@@ -1,47 +1,79 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import time
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from bs4 import BeautifulSoup as bs
 import random
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(executable_path='chromedriver', options=chrome_options)
-driver.wait = WebDriverWait(driver, 10)
+
+driver = webdriver.Chrome(executable_path='./chromedriver', options=chrome_options)
 
 
 counter = 0
-url = driver.get("https://twitter.com/search?q=%23Winter")
+url = driver.get('https://twitter.com/search?q=%23Winter')
 formated_tags = []
 
-
 def scrape_twitter(url, counter, formated_tags):
-    try:
-        element = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//a[contains(@href,'hashtag')]"))
-        )
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//a[contains(@href,'hashtag')]"))
+            )
 
-    finally:
-        tags = driver.find_elements_by_xpath("//a[contains(@href,'hashtag')]")
-        for tag in tags:
-            print(f"----{tag.get_attribute('href')}")
-            formated_tags.append(tag.get_attribute('href'))
+        finally:
+            tags = driver.find_elements_by_xpath("//a[contains(@href,'hashtag')]")
+            for tag in tags:
+                print(f"----{tag.get_attribute('href')}")
+                formated_tags.append(tag.get_attribute('href'))
 
-        print(6 - counter, "pages to visit")
 
-        url = random.choice(formated_tags)
-        url = str(url)
-        print("next url to visit: ", str(url))
+            print(6 - counter, "pages to visit")
 
-    while counter < 6:
-        counter += 1
-        scrape_twitter(url, counter, formated_tags)
+            url = random.choice(formated_tags)
+            url = str(url)
+            print("next url to visit: ", str(url))
+
+        while counter < 6:
+            counter += 1
+            scrape_twitter(url, counter, formated_tags)
 
 
 scrape_twitter(url, counter, formated_tags)
 
+
 driver.close()
+
+
+"""
+
+
+base_url = u'https://twitter.com/search?q='
+query = u'%23Winter'
+url = base_url + query
+browser.get(url)
+
+for _ in range(10):
+    body.send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.2)
+
+tweets = browser.find_elements_by_css_selector("[data-testid=\"tweet\"]")
+
+bucket = {}
+
+for tweet in tweets:
+    bucket['text'] = tweet.text.format()
+    bucket['url'] = tweet.
+    #print(bucket)
+
+    if len(bucket) < 6:
+        # print(tweet.text)
+        tags = driver.find_elements_by_xpath("//a[contains(@href,'hashtag')]")
+        for tag in tags:
+            print(f"----{tag.get_attribute('href')}")
+            formated_tags.append(tag.get_attribute('href'))
+            # print(formated_tags)
+
+"""
